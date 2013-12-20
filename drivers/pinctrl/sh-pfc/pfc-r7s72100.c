c3300d765dd1c94a87261864c69176f71939e86b
Geert Uytterhoeven <geert+renesas@glider.be>
2013-12-20 11:35:44 +0100

    [WIP] eth pfc comments from Laurent
    Signed-off-by: Geert Uytterhoeven <geert+renesas@glider.be>

--- drivers/pinctrl/sh-pfc/pfc-r7s72100.c
+++ drivers/pinctrl/sh-pfc/pfc-r7s72100.c
@@ -258,14 +258,14 @@ SCIF7(RZ_PIN_AND_MUX)
 	fn(ethernet, txer,   2,  1, 2)		\
 	fn(ethernet, txen,   2,  2, 2)		\
 	fn(ethernet, txcrs,  2,  3, 2)		\
-	fn(ethernet, txd,    2,  4, 2)		\
-	fn(ethernet, txd,    2,  5, 2)		\
-	fn(ethernet, txd,    2,  6, 2)		\
-	fn(ethernet, txd,    2,  7, 2)		\
-	fn(ethernet, rxd,    2,  8, 2)		\
-	fn(ethernet, rxd,    2,  9, 2)		\
-	fn(ethernet, rxd,    2, 10, 2)		\
-	fn(ethernet, rxd,    2, 11, 2)		\
+	fn(ethernet, txd0,   2,  4, 2)		\
+	fn(ethernet, txd1,   2,  5, 2)		\
+	fn(ethernet, txd2,   2,  6, 2)		\
+	fn(ethernet, txd3,   2,  7, 2)		\
+	fn(ethernet, rxd0,   2,  8, 2)		\
+	fn(ethernet, rxd1,   2,  9, 2)		\
+	fn(ethernet, rxd2,   2, 10, 2)		\
+	fn(ethernet, rxd3,   2, 11, 2)		\
 	fn(ethernet, txclk,  3,  0, 2)		\
 	fn(ethernet, txer,   3,  1, 2)		\
 	fn(ethernet, txen,   3,  2, 2)		\
@@ -278,14 +278,14 @@ SCIF7(RZ_PIN_AND_MUX)
 	fn(ethernet, txclk,  7,  1, 3)		\
 	fn(ethernet, txer,   7,  2, 3)		\
 	fn(ethernet, txen,   7,  3, 3)		\
-	fn(ethernet, txd,    7,  4, 3)		\
-	fn(ethernet, txd,    7,  5, 3)		\
-	fn(ethernet, txd,    7,  6, 3)		\
-	fn(ethernet, txd,    7,  7, 3)		\
-	fn(ethernet, rxd,    7,  9, 3)		\
-	fn(ethernet, rxd,    7, 10, 3)		\
-	fn(ethernet, rxd,    7, 11, 2)		\
-	fn(ethernet, rxd,    7, 12, 3)		\
+	fn(ethernet, txd0,   7,  4, 3)		\
+	fn(ethernet, txd1,   7,  5, 3)		\
+	fn(ethernet, txd2,   7,  6, 3)		\
+	fn(ethernet, txd3,   7,  7, 3)		\
+	fn(ethernet, rxd0,   7,  9, 3)		\
+	fn(ethernet, rxd1,   7, 10, 3)		\
+	fn(ethernet, rxd2,   7, 11, 2)		\
+	fn(ethernet, rxd3,   7, 12, 3)		\
 	fn(ethernet, mdio,   7, 13, 3)		\
 	fn(ethernet, crs,    7, 14, 3)		\
 	fn(ethernet, rxclk,  7, 15, 3)		\
@@ -296,14 +296,14 @@ SCIF7(RZ_PIN_AND_MUX)
 	fn(ethernet, txer,  10,  1, 4)		\
 	fn(ethernet, txen,  10,  2, 4)		\
 	fn(ethernet, crs,   10,  3, 4)		\
-	fn(ethernet, txd,   10,  4, 4)		\
-	fn(ethernet, txd,   10,  5, 4)		\
-	fn(ethernet, txd,   10,  6, 4)		\
-	fn(ethernet, txd,   10,  7, 4)		\
-	fn(ethernet, txd,   10,  8, 4)		\
-	fn(ethernet, txd,   10,  9, 4)		\
-	fn(ethernet, txd,   10, 10, 4)		\
-	fn(ethernet, txd,   10, 11, 4)		\
+	fn(ethernet, txd0,  10,  4, 4)		\
+	fn(ethernet, txd1,  10,  5, 4)		\
+	fn(ethernet, txd2,  10,  6, 4)		\
+	fn(ethernet, txd3,  10,  7, 4)		\
+	fn(ethernet, txd0,  10,  8, 4)		\
+	fn(ethernet, txd1,  10,  9, 4)		\
+	fn(ethernet, txd2,  10, 10, 4)		\
+	fn(ethernet, txd3,  10, 11, 4)		\
 
 ETHERNET(RZ_PIN_AND_MUX)
 
