dae8cae75fedeb17050501a713453ad2f487cbcd
Geert Uytterhoeven <geert+renesas@glider.be>
2014-09-10 09:45:28 +0200

    [DEBUG] soc: renesas: r8a7791: MSTP debug handling
    Last updated for R-Car Gen2 datasheet rev. 1.02 (Thx Wolfram!)
    
    Signed-off-by: Geert Uytterhoeven <geert+renesas@glider.be>
    ---
    v2:
      - Use rcar_rst_read_mode_pins().

--- drivers/soc/renesas/renesas-mstp.c
+++ drivers/soc/renesas/renesas-mstp.c
@@ -587,6 +587,12 @@ static int __init proc_mstp_init(void)
 		error = renesas_mstp_setup(&r8a7790_mstp_info);
 	}
 #endif
+#ifdef CONFIG_ARCH_R8A7791
+	if (of_machine_is_compatible("renesas,r8a7791")) {
+		extern const struct renesas_mstp_info r8a7791_mstp_info;
+		error = renesas_mstp_setup(&r8a7791_mstp_info);
+	}
+#endif
 	if (error)
 		return error;
 
