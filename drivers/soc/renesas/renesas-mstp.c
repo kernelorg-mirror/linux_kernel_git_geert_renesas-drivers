61dcbedef1e645f2226c5091407bfda28763502d
Geert Uytterhoeven <geert+renesas@glider.be>
2014-09-10 09:44:08 +0200

    [DEBUG] soc: renesas: r8a7740: MSTP debug handling
    Signed-off-by: Geert Uytterhoeven <geert+renesas@glider.be>
    ---
    v6:
      - Remove unneeded #include <linux/bug.h>,
    
    v5:
      - Add missing SPDX-License-Identifier,
    
    v4:
      - Fill in .mstp_{base,size},
    
    v3:
      - Move extern declaration to renesas-mstp.h,
    
    v2:
      - CONFIG_ARCH_SHMOBILE_MULTI is no more.

--- drivers/soc/renesas/renesas-mstp.c
+++ drivers/soc/renesas/renesas-mstp.c
@@ -614,6 +614,10 @@ static int __init proc_mstp_init(void)
 	if (of_machine_is_compatible("renesas,r8a73a4"))
 		error = renesas_mstp_setup(&r8a73a4_mstp_info);
 #endif
+#ifdef CONFIG_ARCH_R8A7740
+	if (of_machine_is_compatible("renesas,r8a7740"))
+		error = renesas_mstp_setup(&r8a7740_mstp_info);
+#endif
 	if (error)
 		return error;
 
