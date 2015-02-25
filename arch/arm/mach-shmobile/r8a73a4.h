29e69f511e156596b842375db7879cd312e56902
Geert Uytterhoeven <geert+renesas@glider.be>
2015-02-25 13:30:28 +0100

    [DEBUG] ARM: shmobile: r8a73a4: Disable MSTP clocks during boot
    Each module clock has actually three disable bits: one for the System Core
    (ARM CA7/CA15) in an SMSTPCRx register, one for the Realtime Core (SH)
    in an RMSTPRx register, and one for the Modem Core (ARM CR4) in an
    MMSTPRx register Currently we don't touch the bits meant for the
    Realtime Core and Modem cores, so some clocks may inadvertently be
    enabled and still run, wasting power, while they're disabled in the
    SMSTPCRx register.  The actual state of the clock is indicated in the
    corresponding status register.
    
    In addition, some MSTP clocks may inadvertently be enabled before boot up,
    hiding bugs in drivers, and clock / power management core code.
    
    To expose such bugs:
      - Set the disable bits for the Realtime and Modem Cores for all module
        clocks,
      - Set the disable bits for the System Core for all module clocks
        (except for INTC_SY and IRQC, which are always needed, and SCIFA1,
        which may be used for a serial console).
    This is only done when booted from the System Core.
    
    Signed-off-by: Geert Uytterhoeven <geert+renesas@glider.be>
    ---
    v2:
      - Use of_platform_default_populate().

---
+++ arch/arm/mach-shmobile/r8a73a4.h
@@ -0,0 +1,6 @@
+#ifndef __ASM_R8A73A4_H__
+#define __ASM_R8A73A4_H__
+
+void r8a73a4_disable_mstp_clocks(void);
+
+#endif /* __ASM_R8A73A4_H__ */
--- arch/arm/mach-shmobile/setup-r8a73a4.c
+++ arch/arm/mach-shmobile/setup-r8a73a4.c
@@ -7,10 +7,18 @@
  */
 
 #include <linux/init.h>
+#include <linux/of_platform.h>
 
 #include <asm/mach/arch.h>
 
 #include "common.h"
+#include "r8a73a4.h"
+
+static void __init r8a73a4_init_machine(void)
+{
+	r8a73a4_disable_mstp_clocks();
+	of_platform_default_populate(NULL, NULL, NULL);
+}
 
 static const char *const r8a73a4_boards_compat_dt[] __initconst = {
 	"renesas,r8a73a4",
@@ -19,5 +27,6 @@ static const char *const r8a73a4_boards_compat_dt[] __initconst = {
 
 DT_MACHINE_START(R8A73A4_DT, "Generic R8A73A4 (Flattened Device Tree)")
 	.init_late	= shmobile_init_late,
+	.init_machine	= r8a73a4_init_machine,
 	.dt_compat	= r8a73a4_boards_compat_dt,
 MACHINE_END
