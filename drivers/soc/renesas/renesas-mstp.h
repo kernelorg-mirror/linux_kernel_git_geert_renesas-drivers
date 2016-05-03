e20c342f22e1582477551cf633886afa4bd01abd
Geert Uytterhoeven <geert+renesas@glider.be>
2016-05-03 10:05:10 +0200

    [DEBUG] soc: renesas: r8a77960: MSTP debug handling
    Signed-off-by: Geert Uytterhoeven <geert+renesas@glider.be>
    ---
    v9:
      - Remove unneeded #include <linux/bug.h>,
    
    v8:
      - Add missing SPDX-License-Identifier,
    
    v7:
      - Fill in .mstp_{base,size},
    
    v6:
      - Move extern declaration to renesas-mstp.h,
    
    v5:
      - Add missing CRC2/3 and DRIF clocks,
    
    v4:
      - s/r8a7796/r8a77960/,
    
    v3:
      - Add SCIF2,
    
    v2:
      - Use rcar_rst_read_mode_pins(),
      - Sort mstp_clocks[].

--- drivers/soc/renesas/renesas-mstp.h
+++ drivers/soc/renesas/renesas-mstp.h
@@ -69,3 +69,4 @@ extern const struct renesas_mstp_info r8a7740_mstp_info;
 extern const struct renesas_mstp_info r8a7790_mstp_info;
 extern const struct renesas_mstp_info r8a7791_mstp_info;
 extern const struct renesas_mstp_info r8a7795_mstp_info;
+extern const struct renesas_mstp_info r8a77960_mstp_info;
