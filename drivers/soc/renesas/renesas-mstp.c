611d04d50a6c0178cdc4c429afb5983c0d05e979
Geert Uytterhoeven <geert+renesas@glider.be>
2016-05-03 15:41:33 +0200

    [DEBUG] soc: renesas: r8a7793: MSTP debug handling
    Identical to r8a7791.
    
    Signed-off-by: Geert Uytterhoeven <geert+renesas@glider.be>

--- drivers/soc/renesas/renesas-mstp.c
+++ drivers/soc/renesas/renesas-mstp.c
@@ -593,6 +593,12 @@ static int __init proc_mstp_init(void)
 		error = renesas_mstp_setup(&r8a7791_mstp_info);
 	}
 #endif
+#ifdef CONFIG_ARCH_R8A7793
+	if (of_machine_is_compatible("renesas,r8a7793")) {
+		extern const struct renesas_mstp_info r8a7791_mstp_info;
+		error = renesas_mstp_setup(&r8a7791_mstp_info);
+	}
+#endif
 	if (error)
 		return error;
 
