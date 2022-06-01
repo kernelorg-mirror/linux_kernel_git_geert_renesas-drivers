eb3e8b458a668d0cf7fcfdf9159b985a47adecc9
Geert Uytterhoeven <geert+renesas@glider.be>
2022-06-01 15:11:01 +0200

    [WIP] lib: dhry: Add kernel module wrapper
    Signed-off-by: Geert Uytterhoeven <geert+renesas@glider.be>

--- lib/dhry.h
+++ lib/dhry.h
@@ -348,3 +348,5 @@ void Proc_7 (One_Fifty Int_1_Par_Val, One_Fifty Int_2_Par_Val, One_Fifty *Int_Pa
 void Proc_8 (Arr_1_Dim Arr_1_Par_Ref, Arr_2_Dim Arr_2_Par_Ref, int Int_1_Par_Val, int Int_2_Par_Val);
 Enumeration Func_1 (Capital_Letter Ch_1_Par_Val, Capital_Letter Ch_2_Par_Val);
 Boolean Func_2 (Str_30 Str_1_Par_Ref, Str_30 Str_2_Par_Ref);
+
+void dhry (int n);
