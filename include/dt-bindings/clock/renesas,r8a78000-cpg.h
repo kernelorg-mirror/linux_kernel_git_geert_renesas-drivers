924d6c98cbb2f3d8afa9520ff31378a21214889a
Geert Uytterhoeven <geert+renesas@glider.be>
2026-01-23 14:08:53 +0100

    dt-bindings: clock: Document Renesas R-Car X5H Clock Pulse Generator
    Document support for the Renesas R-Car X5H Clock Pulse Generator,
    and add definitions for a very limited and preliminary set of clocks.
    
    Signed-off-by: Geert Uytterhoeven <geert+renesas@glider.be>
    ---
    v2:
      - Add R8A78000_CPG_S0D4_PERE_MAIN, which is the UFS0 functional clock
        parent.

---
+++ include/dt-bindings/clock/renesas,r8a78000-cpg.h
@@ -0,0 +1,16 @@
+/* SPDX-License-Identifier: (GPL-2.0-only OR BSD-2-Clause) */
+/*
+ * Copyright (C) 2026 Glider bv
+ */
+#ifndef __DT_BINDINGS_CLOCK_RENESAS_R8A78000_CPG_H__
+#define __DT_BINDINGS_CLOCK_RENESAS_R8A78000_CPG_H__
+
+/* R-Car X5H CPG Clocks */
+
+// FIXME Preliminary
+#define R8A78000_CPG_SGASYNCD4_PERW_BUS		0
+#define R8A78000_CPG_SGASYNCD16_PERW_BUS	1
+#define R8A78000_CPG_S0D4_PERE_MAIN		2
+#define R8A78000_CPG_MSOCK_PERW_BUS		3
+
+#endif /* __DT_BINDINGS_CLOCK_RENESAS_R8A78000_CPG_H__ */
