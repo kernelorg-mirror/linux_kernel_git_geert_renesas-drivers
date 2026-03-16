7c4e0f646456e09e2196b0277c355477d0cfe2f0
Geert Uytterhoeven <geert+renesas@glider.be>
2026-03-16 16:08:43 +0100

    [FIXME] firmware: arm_scmi: quirk: Handle bad clocks on R-Car X5H
    Renesas R-Car X5H SCP FW SDKv4.28.0, v4.31.0, and v4.32.0 expose a few
    clocks that do not support the CLOCK_ATTRIBUTES or CLOCK_DESCRIBE_RATES
    command.  Since commit 0d8b0c8068a8ff5f ("firmware: arm_scmi: Harden
    clock protocol initialization") in scmi/for-linux-next (next-20260319
    and later), scmi_clock_attributes_get() or
    scmi_clock_describe_rates_get() failures are no longer ignored, but
    prevent the SCMI clock driver from initializing.
    
    Hence add a quirk to ignore such failures, like before.  As the quirk
    handling is harmless for unaffected systems, make it generic for all
    R-Car X5H systems using SCP FW SDKv4.28.0 or later.
    
    FIXME Since commit 0d8b0c8068a8ff5f was dropped, CLOCK_ATTRIBUTES and
    CLOCK_DESCRIBE_RATES failures are no longer fatal.
    
    Signed-off-by: Geert Uytterhoeven <geert+renesas@glider.be>

--- drivers/firmware/arm_scmi/clock.c
+++ drivers/firmware/arm_scmi/clock.c
@@ -1226,6 +1226,18 @@ static const struct scmi_protocol_events clk_protocol_events = {
 	.num_events = ARRAY_SIZE(clk_events),
 };
 
+#define QUIRK_RCAR_X5H_NO_ATTRIBUTES					\
+	({								\
+		if (ret == -EREMOTEIO || ret == -EOPNOTSUPP)		\
+			continue;					\
+	})
+
+#define QUIRK_RCAR_X5H_NO_RATES						\
+	({								\
+		if (ret == -EOPNOTSUPP)					\
+			ret = 0;					\
+	})
+
 static int scmi_clock_protocol_init(const struct scmi_protocol_handle *ph)
 {
 	int clkid, ret;
@@ -1250,8 +1262,12 @@ static int scmi_clock_protocol_init(const struct scmi_protocol_handle *ph)
 	for (clkid = 0; clkid < cinfo->num_clocks; clkid++) {
 		cinfo->clkds[clkid].id = clkid;
 		ret = scmi_clock_attributes_get(ph, clkid, cinfo);
+		// FIXME CLOCK_ATTRIBUTES failures are no longer fatal
+		SCMI_QUIRK(clock_rcar_x5h_no_attributes, QUIRK_RCAR_X5H_NO_ATTRIBUTES);
 		if (!ret)
 			scmi_clock_describe_rates_get(ph, clkid, cinfo);
+		// FIXME CLOCK_DESCRIBE_RATES failures are no longer fatal
+		SCMI_QUIRK(clock_rcar_x5h_no_attributes, QUIRK_RCAR_X5H_NO_RATES);
 	}
 
 	if (PROTOCOL_REV_MAJOR(ph->version) >= 0x3) {
