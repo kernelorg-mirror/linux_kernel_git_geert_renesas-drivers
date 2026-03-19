2b9cc5b41afcfa57194a18f54cef81f99e916158
Geert Uytterhoeven <geert+renesas@glider.be>
2026-03-19 14:38:31 +0100

    firmware: arm_scmi: quirk: Handle critical clocks on R-Car X5H
    Renesas R-Car X5H SCP FW SDKv4.28.0, v4.31.0, and v4.32.0 advertise a
    few clocks that crash the system when disabled.  Add quirks to prevent
    such crashes.
    
    As SCMI clock IDs are identical for SDKv4.31.0 and v4.32.0, the quirk
    for these two versions can be shared.
    
    Signed-off-by: Geert Uytterhoeven <geert+renesas@glider.be>
    ---
    Some of these could be handled by improving hardware descriptions in DT,
    or by the CPG and MDLC drivers.
    
    v2:
      - Mark all Cortex-A related clocks as do-not-touch . Those clocks supply
        CPU cores and clusters and are managed purely by TFA BL31. CPU cores
        and clusters are managed by Linux purely via PSCI interface.
    
        The clocks marked here are CLK_ZC0/ZC1/ZD_APUn core clocks and
        MDLC APRTMGINTn modules.
    
        Signed-off-by: Marek Vasut <marek.vasut+renesas@mailbox.org>

--- drivers/firmware/arm_scmi/clock.c
+++ drivers/firmware/arm_scmi/clock.c
@@ -914,6 +914,26 @@ scmi_clock_config_set_v2(const struct scmi_protocol_handle *ph, u32 clk_id,
 	return ret;
 }
 
+#define QUIRK_RCAR_X5H_4_28_CRIT_CLOCKS					\
+	({								\
+		switch (clk_id) {					\
+		case 468:		/* MDLC_INTAP0 */		\
+		case 498 ... 505:	/* MDLC_APRTMGINT0..7 */	\
+		case 838 ... 861:	/* CLK_ZC0/ZC1/ZD_APU0..7 */	\
+			return -EPERM;					\
+		}							\
+	})
+
+#define QUIRK_RCAR_X5H_4_31_CRIT_CLOCKS					\
+	({								\
+		switch (clk_id) {					\
+		case 464:		/* MDLC_INTAP0 */		\
+		case 494 ... 501:	/* MDLC_APRTMGINT0..7 */	\
+		case 834 ... 857:	/* CLK_ZC0/ZC1/ZD_APU0..7 */	\
+			return -EPERM;					\
+		}							\
+	})
+
 static int scmi_clock_enable(const struct scmi_protocol_handle *ph, u32 clk_id,
 			     bool atomic)
 {
@@ -943,6 +963,9 @@ static int scmi_clock_disable(const struct scmi_protocol_handle *ph, u32 clk_id,
 
 	if (clk->state_ctrl_forbidden)
 		return -EACCES;
+
+	SCMI_QUIRK(clock_rcar_x5h_4_28_crit_clocks, QUIRK_RCAR_X5H_4_28_CRIT_CLOCKS);
+	SCMI_QUIRK(clock_rcar_x5h_4_31_crit_clocks, QUIRK_RCAR_X5H_4_31_CRIT_CLOCKS);
 
 	return ci->clock_config_set(ph, clk_id, CLK_STATE_DISABLE,
 				    NULL_OEM_TYPE, 0, atomic);
